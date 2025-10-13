b001d81e2a4772788e93f44d8f67ef90bac4f27f
. Gameplay <04astapov04@gmail.com>
2025-10-13 16:55:36 +0700

    Constructors

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-using namespace std;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+#define delimiter "\n------------------------------------\n"
 
 class Point
 {
@@ -22,18 +26,58 @@ public:
 	{
 		this->y = y;
 	}
+	//		Constructors:
+	/*Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "SingleArgConstructor:\t" << this << endl;
+	}*/
+	Point(double x = 0, double y = 0)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	// copy constructor
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+	//		Operators:
+	void operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t\t" << this << endl;
+	}
 
 	//		Methods:
-	double distance(Point other)
+	double distance(Point& other)
 	{
 		double x_distance = this->x - other.x;
 		double y_distance = this->y - other.y;
 		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
 		return distance;
 	}
+	void print()const
+	{
+		cout << "X = " << x << ",\tY = " << y << endl;
+	}
 };
 
-double distance(Point A, Point B)
+double distance(const Point& A, const Point& B)
 {
 	double x_distance = A.get_x() - B.get_x();
 	double y_distance = A.get_y() - B.get_y();
@@ -43,6 +87,10 @@ double distance(Point A, Point B)
 
 
 //#define STRUCT_POINT
+//#define DISTANCE_CHECK
+//#define FOR_COUNTER_LIFETIME
+//#define CONSTRUCTORS_CHECK
+#define ASSIGNMENT_CHECK
 
 void main()
 {
@@ -59,6 +107,7 @@ void main()
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUCT_POINT //ctrl + k + s для ifdef
 
+#ifdef DISTANCE_CHECK
 	Point A;
 	A.set_x(2);
 	A.set_y(3);
@@ -70,7 +119,55 @@ void main()
 	cout << B.get_x() << "\t" << B.get_y() << endl;
 
 	cout << "Расстояние от точки 'A' до точки 'B': " << A.distance(B) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние от точки 'B' до точки 'A': " << B.distance(A) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние между точками 'A' и 'B': " << distance(A, B) << endl;
+	cout << delimiter << endl;
 	cout << "Расстояние между точками 'B' и 'A': " << distance(B, A) << endl;
-}																			
+	cout << delimiter << endl;
+#endif // DISTANCE_CHECK
+
+#ifdef FOR_CONTER_LIFETIME
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i << "\t";
+	}
+	// Если бы переменная i была объектом, то по завершении всех операций для неё
+	// был бы вызван distructor, посколько время жизни i завершается завершению всех итераций.
+	cout << endl;
+#endif // FOR_CONTER_LIFETIME
+
+#ifdef CONSTRUCTORS_CHECK
+	Point A;
+	A.print();
+
+	Point B = 5;
+	B.print();
+
+	Point C(2, 3);
+	C.print();
+
+	Point D = C;
+	D.print();
+
+	Point E;
+	E = D;
+	E.print();
+
+#endif // CONSTRUCTORS_CHECK
+
+#ifdef ASSIGNMENT CHECK
+	int a, b, c;
+	a = b = c = 0;
+	cout << a << tab << b << tab << c << endl;
+
+	Point A, B, C;
+	A = B = C = Point(2, 3);
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSIGNMENT CHECK
+
+
+}
